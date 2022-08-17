a96361a3dbdec072a7fffe27f85bbf25b5e69b36
azmi romzi <azmiromzihoesaini@gmail.com>
2022-08-17 23:24:46 +0700

    register sudah bisa masuk ke db

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,113 +27,125 @@
                     <div class="card-body p-md-5 text-black">
                       <h3 class="mb-5 text-uppercase">Student registration form</h3>
 
-                      <div class="row">
-                        <div class="col-md-6 mb-4">
-                          <div class="form-outline">
-                            <input type="text" id="form3Example1m" class="form-control form-control-lg" />
-                            <label class="form-label" for="form3Example1m">First name</label>
-                          </div>
-                        </div>
-                        <div class="col-md-6 mb-4">
-                          <div class="form-outline">
-                            <input type="text" id="form3Example1n" class="form-control form-control-lg" />
-                            <label class="form-label" for="form3Example1n">Last name</label>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="row">
-                        <div class="col-md-6 mb-4">
-                          <div class="form-outline">
-                            <input type="text" id="form3Example1m1" class="form-control form-control-lg" />
-                            <label class="form-label" for="form3Example1m1">Mother's name</label>
-                          </div>
-                        </div>
-                        <div class="col-md-6 mb-4">
-                          <div class="form-outline">
-                            <input type="text" id="form3Example1n1" class="form-control form-control-lg" />
-                            <label class="form-label" for="form3Example1n1">Father's name</label>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div class="form-outline mb-4">
-                        <input type="text" id="form3Example8" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example8">Address</label>
-                      </div>
-
-                      <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
-
-                        <h6 class="mb-0 me-4">Gender: </h6>
-
-                        <div class="form-check form-check-inline mb-0 me-4">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
-                            value="option1" />
-                          <label class="form-check-label" for="femaleGender">Female</label>
+                      <form action="{{ route('register') }}" method="post" >
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-6 mb-4">
+                                <div class="form-outline">
+                                    <input type="text" id="form3Example1m" name="name" class="form-control form-control-lg" />
+                                    <label class="form-label" for="form3Example1m">First name</label>
+                                </div>
+                            </div>
+                            {{-- <div class="col-md-6 mb-4">
+                                <div class="form-outline">
+                                    <input type="text" id="form3Example1n" class="form-control form-control-lg" />
+                                    <label class="form-label" for="form3Example1n">Last name</label>
+                                </div>
+                            </div> --}}
                         </div>
 
-                        <div class="form-check form-check-inline mb-0 me-4">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
-                            value="option2" />
-                          <label class="form-check-label" for="maleGender">Male</label>
+                        {{-- <div class="row">
+                            <div class="col-md-6 mb-4">
+                            <div class="form-outline">
+                                <input type="text" id="form3Example1m1" class="form-control form-control-lg" />
+                                <label class="form-label" for="form3Example1m1">Mother's name</label>
+                            </div>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                            <div class="form-outline">
+                                <input type="text" id="form3Example1n1" class="form-control form-control-lg" />
+                                <label class="form-label" for="form3Example1n1">Father's name</label>
+                            </div>
+                            </div>
                         </div>
 
-                        <div class="form-check form-check-inline mb-0">
-                          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
-                            value="option3" />
-                          <label class="form-check-label" for="otherGender">Other</label>
+                        <div class="form-outline mb-4">
+                            <input type="text" id="form3Example8" class="form-control form-control-lg" />
+                            <label class="form-label" for="form3Example8">Address</label>
                         </div>
 
-                      </div>
+                        <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
 
-                      <div class="row">
-                        <div class="col-md-6 mb-4">
+                            <h6 class="mb-0 me-4">Gender: </h6>
 
-                          <select class="select">
-                            <option value="1">State</option>
-                            <option value="2">Option 1</option>
-                            <option value="3">Option 2</option>
-                            <option value="4">Option 3</option>
-                          </select>
+                            <div class="form-check form-check-inline mb-0 me-4">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
+                                value="option1" />
+                            <label class="form-check-label" for="femaleGender">Female</label>
+                            </div>
+
+                            <div class="form-check form-check-inline mb-0 me-4">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
+                                value="option2" />
+                            <label class="form-check-label" for="maleGender">Male</label>
+                            </div>
+
+                            <div class="form-check form-check-inline mb-0">
+                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
+                                value="option3" />
+                            <label class="form-check-label" for="otherGender">Other</label>
+                            </div>
 
                         </div>
-                        <div class="col-md-6 mb-4">
 
-                          <select class="select">
-                            <option value="1">City</option>
-                            <option value="2">Option 1</option>
-                            <option value="3">Option 2</option>
-                            <option value="4">Option 3</option>
-                          </select>
+                        <div class="row">
+                            <div class="col-md-6 mb-4">
 
+                            <select class="select">
+                                <option value="1">State</option>
+                                <option value="2">Option 1</option>
+                                <option value="3">Option 2</option>
+                                <option value="4">Option 3</option>
+                            </select>
+
+                            </div>
+                            <div class="col-md-6 mb-4">
+
+                            <select class="select">
+                                <option value="1">City</option>
+                                <option value="2">Option 1</option>
+                                <option value="3">Option 2</option>
+                                <option value="4">Option 3</option>
+                            </select>
+
+                            </div>
                         </div>
-                      </div>
 
-                      <div class="form-outline mb-4">
-                        <input type="text" id="form3Example9" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example9">DOB</label>
-                      </div>
+                        <div class="form-outline mb-4">
+                            <input type="text" id="form3Example9" class="form-control form-control-lg" />
+                            <label class="form-label" for="form3Example9">DOB</label>
+                        </div>
 
-                      <div class="form-outline mb-4">
-                        <input type="text" id="form3Example90" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example90">Pincode</label>
-                      </div>
+                        <div class="form-outline mb-4">
+                            <input type="text" id="form3Example90" class="form-control form-control-lg" />
+                            <label class="form-label" for="form3Example90">Pincode</label>
+                        </div>
 
-                      <div class="form-outline mb-4">
-                        <input type="text" id="form3Example99" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example99">Course</label>
-                      </div>
+                        <div class="form-outline mb-4">
+                            <input type="text" id="form3Example99" class="form-control form-control-lg" />
+                            <label class="form-label" for="form3Example99">Course</label>
+                        </div> --}}
 
-                      <div class="form-outline mb-4">
-                        <input type="text" id="form3Example97" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example97">Email ID</label>
-                      </div>
+                        <div class="form-outline mb-4">
+                            <input type="email" name="email" id="form3Example97" class="form-control form-control-lg" />
+                            <label class="form-label" for="form3Example97">Email ID</label>
+                        </div>
 
-                      <div class="d-flex justify-content-end pt-3">
-                        <button type="button" class="btn btn-light btn-lg">Reset all</button>
-                        <button type="button" class="btn btn-warning btn-lg ms-2">Submit form</button>
-                      </div>
+                        <div class="form-outline mb-4">
+                            <input type="password" name="password"  class="form-control form-control-lg" />
+                            <label class="form-label">password </label>
+                        </div>
+                        <div class="form-outline mb-4">
+                            <input type="password" name="password_confirmation"  class="form-control form-control-lg" />
+                            <label class="form-label" >password confirmasi</label>
+                        </div>
+                        <button type="submit"></button>
 
+                        <div class="d-flex justify-content-end pt-3">
+                            <button type="button" class="btn btn-light btn-lg">Reset all</button>
+                            <button type="button" class="btn btn-warning btn-lg ms-2">Submit form</button>
+                        </div>
+                     </form>
                     </div>
                   </div>
                 </div>
