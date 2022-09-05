55cdc5e495f7fd654c657f17547272af89793b99
MUSHANIF <hanifmusthafa2005@gmail.com>
2022-09-05 14:51:01 +0700

    merubah syntax search

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -3,7 +3,7 @@
 <div class="main-content">
     <main>
         @if (Auth::user()->level == 2)
-        <form action="{{ url('siswawali') }}" method="GET" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <form action="{{ url('siswawali') }}" method="GET" class="">
             @csrf
             <div class="input-group">
                 <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
--- resources/views/wali_kelas/index.blade.php
+++ resources/views/wali_kelas/index.blade.php
@@ -2,7 +2,7 @@
 @section('search')
 <div class="main-content">
     <main>
-        <form action="{{ url('wali_kelas') }}" method="GET" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <form action="{{ url('wali_kelas') }}" method="GET" class="">
             @csrf
             <div class="input-group">
                 <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
